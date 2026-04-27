94ff265bb42d9b0971af5a7d83963f999b9a8440
LuisSante <lsantet@unsa.edu.pe>
2026-04-27 20:56:34 -0300

    feat: 🎸 advance

--- server/utils/config.py
+++ server/utils/config.py
@@ -52,7 +52,7 @@ class Config:
   REFERENCE_PATTERNS = [
     ("section", re.compile(r'\b[Ss]ection\s+(\d+(?:\.\d+)*)\b')),
     ("article", re.compile(r'\b[Aa]rticle\s+(\d+(?:\.\d+)*)\b')),
-    ("exhibit", re.compile(r'\b[Ee]xhibit\s+([A-Za-z]|\d+(?:\.\d+)*)\b')),
+    # ("exhibit", re.compile(r'\b[Ee]xhibit\s+([A-Za-z]|\d+(?:\.\d+)*)\b')),
     ("schedule", re.compile(r'\b[Ss]chedule\s+([A-Za-z]|\d+(?:\.\d+)*)\b')),
     ("annex",    re.compile(r'\b[Aa]nnex\s+([A-Za-z]|\d+(?:\.\d+)*)\b')),
     ("appendix", re.compile(r'\b[Aa]ppendix\s+([A-Za-z]|\d+(?:\.\d+)*)\b')),
